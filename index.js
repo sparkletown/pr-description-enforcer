877c4f50f83d96358c522546831200321a977eff
derkinderfietsen <20744388+derkinderfietsen@users.noreply.github.com>
2020-10-11 00:56:29 +0300

    V1 (#4)
    * fix octokit version
    
    * bump
    
    * add check to pull_request_template

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const PR_TEMPLATE_PATHS = [
+  'PULL_REQUEST_TEMPLATE.md',
+  'pull_request_template.md',
+  '.github/PULL_REQUEST_TEMPLATE.md',
+  '.github/pull_request_template.md',
+  'docs/PULL_REQUEST_TEMPLATE.md',
+  'docs/pull_request_template.md',
+  'PULL_REQUEST_TEMPLATE.txt',
+  'pull_request_template.txt',
+  '.github/PULL_REQUEST_TEMPLATE.txt',
+  '.github/pull_request_template.txt',
+  'docs/PULL_REQUEST_TEMPLATE.txt',
+  'docs/pull_request_template.txt',
+  'PULL_REQUEST_TEMPLATE',
+  'pull_request_template',
+  '.github/PULL_REQUEST_TEMPLATE',
+  '.github/pull_request_template',
+  'docs/PULL_REQUEST_TEMPLATE',
+  'docs/pull_request_template',
+]
+
 const getPrNumber = () => {
   const pullRequest = github.context.payload.pull_request;
   if (!pullRequest) {
@@ -15,39 +36,38 @@ const getPrDescription = async (client) => {
     core.error("Could not get pull request number from context, exiting");
     return;
   }
-
   const { data: pullRequest } = await client.pulls.get({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
     pull_number: prNumber
   });
 
-  core.info(`pr: ${JSON.stringify(pullRequest)}`);
-
-  return pullRequest.body && pullRequest.body.trim()
+  return pullRequest.body.replace(/(\r\n)/g, '\n')
 }
 
-const getPrTemplate = async (client) => {
-  const {data: {files: {pull_request_template}}} = await client.repos.getCommunityProfileMetrics({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-  });
+const getPrTemplate = async (client, paths) => {
+  const prTemplatePath = paths.shift();
 
-  core.info(`pr template url: ${pull_request_template.url}`);
+  core.info(`trying pr template path: ${prTemplatePath}`);
 
-  const prTemplatePath = pull_request_template.url.split('/').splice(7).join('/');
+  try {
+    const {data: content} = await client.repos.getContent({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      path: prTemplatePath,
+      mediaType: {
+        format: 'raw',
+      } 
+    });
 
-  core.info(`pr template path: ${prTemplatePath}`);
-
-  const {data: prTemplateContents} = await client.repos.getContent({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    prTemplatePath,
-  });
-
-  core.info(`pr template content: ${prTemplateContents}`);
-
-  return prTemplateContents.trim();
+    return content;
+  } catch (error) {
+    if (!paths.length) {
+      return undefined
+    }
+    core.warning(`error getting pr template (${prTemplatePath}): ${error.message}`)
+    return getPrTemplate(client, paths)
+  }
 }
 
 // most @actions toolkit packages have async methods
@@ -55,10 +75,10 @@ async function run() {
   try {
     const token = core.getInput("repo-token", { required: true });
 
-    const client = new github.GitHub(token);
+    const client = github.getOctokit(token)
 
     const prDescription = await getPrDescription(client)
-    const prTemplate = await getPrTemplate(client)
+    const prTemplate = await getPrTemplate(client, PR_TEMPLATE_PATHS)
 
     if (!prDescription || prDescription === prTemplate) {
       core.setFailed('PR description missing');
